c45ca5ae8f06401ee0bd37b469b51ccc77faa581
Yeewon <yes3427@kookmin.ac.kr>
2021-06-08 13:06:03 +0900

    feat: webpack 설정

---
+++ public/index_bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/***/ (() => {
+
+eval("const headerTemplate = `\r\n<a href=\"/\" class=\"text-black\">\r\n  <h1 class=\"text-center font-bold\">🚇 지하철 노선도</h1>\r\n</a>\r\n<nav class=\"d-flex justify-center flex-wrap\">\r\n  <a href=\"/pages/stations.html\" class=\"my-1\">\r\n    <button class=\"btn bg-white shadow mx-1\">🚉 역 관리</button>\r\n  </a>\r\n  <a href=\"/pages/lines.html\" class=\"my-1\">\r\n    <button class=\"btn bg-white shadow mx-1\">🛤️ 노선 관리</button>\r\n  </a>\r\n  <a href=\"/pages/sections.html\" class=\"my-1\">\r\n    <button class=\"btn bg-white shadow mx-1\">🔁 구간 관리</button>\r\n  </a>\r\n  <a href=\"/pages/login.html\" class=\"my-1\">\r\n    <button class=\"btn bg-white shadow mx-1\">👤 로그인</button>\r\n  </a>\r\n</nav>`;\r\n\r\ndocument.querySelector('header').innerHTML = headerTemplate;\r\n\n\n//# sourceURL=webpack://js-subway-admin/./src/js/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/js/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name]_bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: './index.html',
+            chunks: ['index'],
+        }),
+    ],
+};
