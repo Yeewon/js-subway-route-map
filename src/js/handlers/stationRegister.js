dd874cd76dbecd684a3ba2c5d83ca9a852ea9553
Yeewon <yes3427@kookmin.ac.kr>
2021-06-23 01:44:30 +0900

    feat: 역 이름 수정 및 삭제 기능

--- src/js/handlers/stationRegister.js
+++ src/js/handlers/stationRegister.js
@@ -1,26 +1,60 @@
-import {navigationTemplate} from '../templates/navigation.js';
-import {MIN_STATION_NAME_MSG, MAX_STATION_NAME_MSG, REMOVE_CONFIRM_MSG} from '../constants/message.js';
+import {MIN_STATION_NAME_MSG, MAX_STATION_NAME_MSG, CONFIRM_MSG} from '../constants/message.js';
 import {stations} from '../states/stations.js';
+import {MIN_STATION_NAME_LENGTH, MAX_STATION_NAME_LENGTH} from '../constants/constant.js';
 import {$} from '../utils/DOM.js';
 
-const initState = () => {
+export let targetStation = '';
+
+export const initState = () => {
     stations.init();
 };
 
+export const initEvent = () => {
+    $('#station-register-form').addEventListener('submit', handleStationRegister);
+    $('#station-name-update-form').addEventListener('submit', handleStationUpdate);
+    $('#stationList').addEventListener('click', handleStationControl);
+    $('.modal-close').addEventListener('click', onModalClose);
+};
+
+const onModalClose = () => {
+    $('.modal').classList.remove('open');
+};
+
 const checkNameRegulations = (stationName) => {
-    if (stationName.length < 2) return alert(MIN_STATION_NAME_MSG);
-    if (stationName.length > 20) return alert(MAX_STATION_NAME_MSG);
+    if (stationName.length < MIN_STATION_NAME_LENGTH) return alert(MIN_STATION_NAME_MSG);
+    if (stationName.length > MAX_STATION_NAME_LENGTH) return alert(MAX_STATION_NAME_MSG);
     return true;
 };
+
 const handleStationRegister = (e) => {
     e.preventDefault();
-    console.log(e.target);
+
     const stationName = e.target.elements['station-name-input'].value;
 
+    e.target.elements['station-name-input'].value = '';
     if (!checkNameRegulations(stationName)) return;
     stations.add(stationName);
 };
 
-document.querySelector('header').innerHTML = navigationTemplate;
-window.addEventListener('submit', handleStationRegister);
-initState();
+const handleStationUpdate = (e) => {
+    e.preventDefault();
+
+    stations.update(targetStation);
+};
+
+const handleStationControl = (e) => {
+    e.preventDefault();
+
+    if (e.target.id === 'update') {
+        targetStation = e.target.closest('li').querySelector('#station-name').innerText;
+
+        $('#station-name-update-input').value = targetStation;
+        $('.modal').classList.add('open');
+    }
+
+    if (e.target.id === 'remove') {
+        const targetStationName = e.target.closest('li').querySelector('#station-name').innerText;
+        if (!window.confirm(CONFIRM_MSG.station)) return;
+        stations.remove(targetStationName);
+    }
+};
