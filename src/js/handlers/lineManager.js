41908c4fa64936687a4032684f575825ccd2693b
Yeewon <yes3427@kookmin.ac.kr>
2022-02-14 14:40:35 +0900

    feat: 노선 추가 및 수정 기능 구현

--- src/js/handlers/lineManager.js
+++ src/js/handlers/lineManager.js
@@ -6,14 +6,19 @@ import { getLocalStorage } from '../utils/localStorage.js';
 import { REMOVE_CONFIRM_MSG } from '../constants/message.js';
 import { lines } from '../states/lines.js';
 import { checkNameRegulations } from '../utils/checkRegulations.js';
+import { onModalClose, onModalShow } from '../ui/modal/index.js';
+import { renderLineModalButton } from '../viewController/subway.js';
+
+let targetLineName = null;
 
 export const initState = () => {
   setColorSelector();
-  setModalInfo();
+  setStationSelect();
   lines.init();
 };
 
 export const initEvent = () => {
+  $('.create-line-btn').addEventListener('click', onRegisterModalShow);
   $('#line-register-form').addEventListener('submit', handleLineRegister);
   $('.subway-line-color-selector').addEventListener(
     'click',
@@ -30,29 +35,27 @@ const handleLineRegister = e => {
   if (!checkNameRegulations(lineName)) return;
 
   const newLine = setLineInfo();
-  lines.add(newLine);
+  console.log(targetLineName);
+  targetLineName ? lines.update(targetLineName, newLine) : lines.add(newLine);
+  onModalClose();
 };
 
-const handleLineControl = e => {
-  e.preventDefault();
+const handleLineControl = ({ target }) => {
+  const type = target.id;
+  if (!['update', 'remove'].includes(type)) return;
 
-  if (e.target.id === 'update') return handleLineUpdate(e);
-  else if (e.target.id === 'remove') return handleLineRemove(e);
-};
-
-const handleLineUpdate = e => {
-  const targetLineName = e.target
+  targetLineName = target
     .closest('li')
     .querySelector('.subway-line-list-item-name').innerText;
-  lines.update(targetLineName);
-};
 
-const handleLineRemove = e => {
-  const targetLineName = e.target
-    .closest('li')
-    .querySelector('.subway-line-list-item-name').innerText;
-  if (!window.confirm(REMOVE_CONFIRM_MSG.line)) return;
-  lines.remove(targetLineName);
+  if (type === 'update') {
+    onModalShow();
+    setUpdateModal(targetLineName);
+  }
+  if (type === 'remove') {
+    if (!window.confirm(REMOVE_CONFIRM_MSG.line)) return;
+    lines.remove(targetLineName);
+  }
 };
 
 const handleColorSelector = e => {
@@ -60,13 +63,7 @@ const handleColorSelector = e => {
   $('#subway-line-color').value = color;
 };
 
-const setColorSelector = () => {
-  $('.subway-line-color-selector').innerHTML = colorOptions
-    .map(subwayLineColorOptionTemplate)
-    .join('');
-};
-
-const setModalInfo = () => {
+const setStationSelect = () => {
   const stationList = getLocalStorage(STATION_LIST);
   $('#up-station').insertAdjacentHTML(
     'beforeend',
@@ -76,6 +73,12 @@ const setModalInfo = () => {
     'beforeend',
     createStationSelectTemplate(stationList),
   );
+};
+
+const setColorSelector = () => {
+  $('.subway-line-color-selector').innerHTML = colorOptions
+    .map(subwayLineColorOptionTemplate)
+    .join('');
 };
 
 const subwayLineColorOptionTemplate = (color, index) => {
@@ -94,3 +97,25 @@ const setLineInfo = () => ({
   color: $('#subway-line-color').value,
   section: [],
 });
+
+const setUpdateModal = targetLineName => {
+  const targetLineInfo = lines.getLineInfo(targetLineName);
+  const { name, upStation, downStation, distance, duration, color } =
+    targetLineInfo;
+
+  renderLineModalButton('update');
+
+  $('#modal-title').innerText = '🛤️ 노선 수정';
+  $('#subway-line-name').value = name;
+  $('#up-station').value = upStation;
+  $('#down-station').value = downStation;
+  $('#distance').value = distance;
+  $('#duration').value = duration;
+  $('#subway-line-color').value = color;
+};
+
+const onRegisterModalShow = () => {
+  targetLineName = null;
+  renderLineModalButton('register');
+  $('#line-register-form').reset();
+};
