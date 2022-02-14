58b1d2f2953d1a40bff6dec0d48c99bfd65fbeb8
Yeewon <yes3427@kookmin.ac.kr>
2022-02-14 14:39:31 +0900

    feat: 노선 관리 모달 추가 및 수정 버튼 템플릿 추가

---
+++ src/js/templates/lineModalSubmitButton.js
@@ -0,0 +1,11 @@
+export const registerButtonTemplate = `
+    <button id="line-register-submit" type="submit" name="line-register-submit" class="input-submit bg-cyan-300">
+        확인
+    </button>
+`;
+
+export const updateButtonTemplate = `
+    <button id="line-update-submit" type="submit" name="line-update-submit" class="input-submit bg-cyan-300">
+        수정
+    </button>
+`;
